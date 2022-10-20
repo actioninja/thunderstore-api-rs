88db3cca4f5a15dd7905d7b4438de789a5061983
actioninja <actioninja@criticalaction.net>
2022-10-20 03:43:05 -0700

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,20 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.                   /
 ////////////////////////////////////////////////////////////////////////////////
 
+#![warn(clippy::pedantic, clippy::cargo)]
+// Default::default() is more idiomatic imo
+#![allow(clippy::default_trait_access)]
+// too many lines is a dumb metric
+#![allow(clippy::too_many_lines)]
+// as is fine, clippy is silly
+#![allow(clippy::cast_lossless)]
+// Not actually going to be a published crate, useless to add
+#![allow(clippy::cargo_common_metadata)]
+// Annoying
+#![allow(clippy::module_name_repetitions)]
+
 #[macro_use]
 extern crate serde_derive;
-
-extern crate reqwest;
-extern crate serde;
-extern crate serde_json;
-extern crate url;
 
 pub mod apis;
 pub mod models;
